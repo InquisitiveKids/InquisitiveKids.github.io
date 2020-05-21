cf66b260d1907b7391febdcbd57f3b59078424d4
KangbinJia <kangbin.jia@outlook.com>
2020-05-21 13:50:37 -0400

    add code

--- kangbin/ilpe.js
+++ kangbin/ilpe.js
@@ -1,13 +1,28 @@
-background(130, 52, 130);
 
-var X = ellipse;
-noStroke();
-var B = 1;
+var sketchProc = function (processingInstance) {
+    with (processingInstance) {
+        size(400, 400);
+        frameRate(30);
+        // KA Javascript code goes here!
 
-draw = function () {
-    fill(mouseY, mouseX, 0);
-    ellipse(mouseX, mouseY, 20, 20);
-    textSize(B);
-    text("W", 0, 321);
-    B = B + 1;
+        // ----------  Begin   ------------ 
+
+
+        background(130, 52, 130);
+
+        var X = ellipse;
+        noStroke();
+        var B = 1;
+
+        draw = function () {
+            fill(mouseY, mouseX, 0);
+            ellipse(mouseX, mouseY, 20, 20);
+            textSize(B);
+            text("W", 0, 321);
+            B = B + 1;
+        };
+
+        // ----------- End   ----------------
+
+    }
 };
